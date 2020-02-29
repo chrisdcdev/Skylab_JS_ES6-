ea1fa351a9382e2ff69fa2538da75a03bc7209df
Christian Castro <alvescastro00cca13@gmail.com>
2020-02-29 16:34:16 -0300

    Arrow Functions

--- bundle.js
+++ bundle.js
@@ -1,35 +1,28 @@
 "use strict";
 
-//////////////////////////////////////////////////////
-//USANDO O map PARA PERCORRER O VETOR E RETORNAR UMA INFORMAÇÃO
+// const arr = [1, 2, 3, 4, 5];
+// const newArr = arr.map(function(item){
+//  return item * 0;
+// });
+// console.log(newArr);
+////////////////////////////////////////////////////////
+//USANDO arrow function
 var arr = [1, 2, 3, 4, 5];
 var newArr = arr.map(function (item) {
-  return item * 0;
+  return item * 2;
 });
-console.log(newArr);
-var arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-var newArr2 = arr2.map(function (item, index) {
-  return item * index;
-});
-console.log(newArr2); ///////////////////////////////////////////////////////
-//USANDO O reduce para consumir todo o vetor e retornar uma única variável
+console.log(newArr); ///////////////////////////////////////////////////////
 
-var arr3 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-var newArr3 = arr3.reduce(function (total, next) {
-  return total + next;
-});
-console.log(newArr3); ///////////////////////////////////////////////////////
-//USANDO O filter PARA FILTRAR UM ARRAY
+var test = function test() {
+  return ["array", "ARRAY"];
+};
 
-var arr4 = [1, 2, 3, 4, 5, 6];
-var newArr4 = arr4.filter(function (item) {
-  return item % 2 === 0;
-});
-console.log(newArr4); /////////////////////////////////////////////////////
-//USANDO O find PARA PROCURAR UM ELEMENTO DENTRO DO ARRAY
+console.log(test());
 
-var arr5 = [1, 2, 3, 4, 5];
-var newArr5 = arr5.find(function (item) {
-  return item === 7;
-});
-console.log(newArr5);
+var test2 = function test2() {
+  return {
+    nome: "Chris"
+  };
+};
+
+console.log(test2());
--- main.js
+++ main.js
@@ -1,41 +1,20 @@
-//////////////////////////////////////////////////////
-//USANDO O map PARA PERCORRER O VETOR E RETORNAR UMA INFORMAÇÃO
-const arr = [1, 2, 3, 4, 5];
+// const arr = [1, 2, 3, 4, 5];
 
-const newArr = arr.map(function(item){
- return item * 0;
-});
+// const newArr = arr.map(function(item){
+//  return item * 0;
+// });
+// console.log(newArr);
+
+////////////////////////////////////////////////////////
+//USANDO arrow function
+
+const arr = [1,2,3,4,5];
+const newArr = arr.map(item => item * 2);
 console.log(newArr);
-
-const arr2 = [1,2,3,4,5,6,7,8,9];
-
-const newArr2 = arr2.map(function(item, index){
-return item * index;
-})
-console.log(newArr2);
 ///////////////////////////////////////////////////////
-//USANDO O reduce para consumir todo o vetor e retornar uma única variável
 
-const arr3 = [1,2,3,4,5,6,7,8,9];
+const test = () => ["array", "ARRAY"];
+console.log(test());
 
-const newArr3 = arr3.reduce(function(total, next){
-    return total + next;
-})
-console.log(newArr3);
-
-///////////////////////////////////////////////////////
-//USANDO O filter PARA FILTRAR UM ARRAY
-
-const arr4 = [1,2,3,4,5,6];
-const newArr4 = arr4.filter(function(item){
-    return item % 2 === 0;
-})
-console.log(newArr4);
-
-/////////////////////////////////////////////////////
-//USANDO O find PARA PROCURAR UM ELEMENTO DENTRO DO ARRAY
-const arr5 = [1,2,3,4,5];
-const newArr5 = arr5.find(function(item){
-    return item === 7; 
-})
-console.log(newArr5);
+const test2 = () => ({nome : "Chris"});
+console.log(test2());
