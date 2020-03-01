7e64a66ead2221cbfd985a1c5918ea664668c66a
Christian Castro <alvescastro00cca13@gmail.com>
2020-03-01 00:02:12 -0300

    operadores rest/spread

--- main.js
+++ main.js
@@ -1,24 +1,49 @@
+//REST
+//serve para pegar o resto das propriedades
+
 const usuario = {
     nome: "Chris",
     idade: 20,
-    endereco: {
-        cidade: "Ocara",
-        estado: "CE"
-    }
+    empresa: "google"
+};
 
-}
-//////////////////////////////////////////////
-//DESESTRUTURAÇÃO
+const {nome, ...resto} = usuario;
 
-const { nome, idade, endereco:{ estado} } = usuario;
 console.log(nome);
-console.log(idade);
-console.log(estado);
-////////////////////////////////////////////////
+console.log(resto);
 
-function mostraNome( { nome , idade } ){
-    console.log(nome, idade);
+//////////////////////////////////////////////////
+const arr = [1,2,3,4,5];
+const [a, b, ...c] = arr;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+//////////////////////////////////////////////////
+
+function soma(...params) {
+    return params.reduce((total, next) => total + next);
 }
-mostraNome(usuario);
 
+console.log(soma(1,2,3,4,5,6,7,8,9));
+
+//SPREAD
+//repassa as informações de um array ou objeto para outra estrutura de dados
+
+const arr1 = [1,2,3];
+const arr2 = [4,5,6];
+
+    const arr3 = [ ...arr1, ...arr2 ];
+    console.log(arr3);
+/////////////////////////////////////////////////////////////
+
+const usuario2 = {
+    nome: "Cecília",
+    idade: 10,
+    empresa: "YouTube"
+};
+
+const usuario3 = {...usuario2, nome: "Gabriel"};
+console.log(usuario3)
 
