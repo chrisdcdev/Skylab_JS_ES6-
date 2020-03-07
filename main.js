baac90fe2ee1a57f46a23f9988dfa3cf6408d8c1
Christian Castro <alvescastro00cca13@gmail.com>
2020-03-07 10:36:44 -0300

    Ex1_m1

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-const nome = "Christian";
-const idade = 20;
+class Usuario{
+    constructor(email, senha){
+        this.email = email;
+        this.senha = senha;
+    }
 
-
-// USO DE Object Short Syntax(Sintaxe curta de objeto)
-
-const dev = {
-    nome, //
-    idade, //
-    empresa: "DevInDev"
+    isAdmin(){
+        return this.admin === true;
+    }
 }
-console.log(dev);
+
+class Admin extends Usuario{
+    constructor(email, senha){
+        super(email, senha);
+        this.admin = true;
+    }
+
+}
+
+const User1 = new Usuario('email@teste.com', 'senha123');
+const Adm1 = new Admin('email@teste.com', 'senha123');
+
+console.log(User1.isAdmin()) // false
+console.log(Adm1.isAdmin()) // true
