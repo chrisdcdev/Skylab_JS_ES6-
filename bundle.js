3aafe3858607d741830f873c8b94748ecd91ced9
Christian Castro <alvescastro00cca13@gmail.com>
2020-03-20 00:33:03 -0300

    import/export

--- bundle.js
+++ bundle.js
@@ -90,11 +90,11 @@
 /*!*******************!*\
   !*** ./funcao.js ***!
   \*******************/
-/*! exports provided: soma */
+/*! exports provided: soma, sub, mult */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"soma\", function() { return soma; });\nfunction soma(a, b) {\n  return a + b;\n}\n\n//# sourceURL=webpack:///./funcao.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"soma\", function() { return soma; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"sub\", function() { return sub; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"mult\", function() { return mult; });\nfunction soma(a, b) {\n  return a + b;\n}\nfunction sub(a, b) {\n  return a - b;\n}\nfunction mult(a, b) {\n  return a * b;\n}\n\n//# sourceURL=webpack:///./funcao.js?");
 
 /***/ }),
 
@@ -106,7 +106,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) *
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _funcao__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./funcao */ \"./funcao.js\");\n\nconsole.log(Object(_funcao__WEBPACK_IMPORTED_MODULE_0__[\"soma\"])(3, 5));\n\n//# sourceURL=webpack:///./main.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _funcao__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./funcao */ \"./funcao.js\");\n\nconsole.log(_funcao__WEBPACK_IMPORTED_MODULE_0__[\"soma\"](10, 10));\nconsole.log(_funcao__WEBPACK_IMPORTED_MODULE_0__[\"sub\"](10, 10));\nconsole.log(_funcao__WEBPACK_IMPORTED_MODULE_0__[\"mult\"](10, 10));\n\n//# sourceURL=webpack:///./main.js?");
 
 /***/ })
 
--- main.js
+++ main.js
@@ -1,2 +1,5 @@
-import {soma} from './funcao';
-console.log(soma(3,5));
+import * as funcao from './funcao';
+
+console.log(funcao.soma(10,10));
+console.log(funcao.sub(10,10));
+console.log(funcao.mult(10,10));
